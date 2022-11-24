bf79c1d0b055da0ece68b99e9f03d3bd55f34185
Miguel-Leite <miguelleite200leite@email.com>
2022-11-24 13:53:46 +0100

    :tada: initialize repository

---
+++ simple-server.ts
@@ -0,0 +1,51 @@
+import { ApolloServer, gql } from 'apollo-server';
+import { randomUUID } from 'node:crypto';
+
+const typeDefs = gql`
+  type User {
+    id: String!
+    name: String!
+  }
+
+  type Query {
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): User!
+  }
+`;
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const users: User[] = [];
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query: {
+      users: () => {
+        return users;
+      }
+    },
+    Mutation: {
+      createUser: (_, args: User) => {
+        const user = {
+          id: randomUUID(),
+          name: args.name
+        }
+        
+        users.push(user)
+
+        return user;
+      }
+    }
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`\u{1F680} HTTP server running on ${url}`)
+})
